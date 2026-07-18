dc03af4a6b43008e5e0017b32f5f52a002211a3a
alexanderbertoni9-sudo <alexander.bertoni9@gmail.com>
2026-07-17 22:31:24 -0400

    updated it to now show value of all current scores

--- main.js
+++ main.js
@@ -15,6 +15,29 @@ classCount();
 // Actual Code
 
 function returnScores(){
-    var scores = {};
+    var items = document.getElementsByClassName("apscores-card");
 
+    for (var i = 0; i < items.length; i++) {
+        var parent = items[i];
+        var children = parent.getElementsByClassName("apscores-badge");
+
+        for (var j = 0; j < children.length; j++) {
+            var child = children[j];
+
+            /* loop through all child nodes (including text nodes) */
+            for (var k = 0; k < child.childNodes.length; k++) {
+                var node = child.childNodes[k];
+
+                /* nodeType 3 = text node */
+                if (node.nodeType === 3) {
+                    var value = node.textContent.trim();
+                    if (value) {
+                        console.log(value);
+                    }
+                }
+            }
+        }
+    }
 }
+
+returnScores();
